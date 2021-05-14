60d0176397a7dbeae4852ad54641330ae8468288
Slayer2281 <82646621+Slayer2281@users.noreply.github.com>
2021-05-14 16:22:22 +0700

    =)

--- script.js
+++ script.js
@@ -13,7 +13,12 @@ document.getElementById("color").oninput = function(){
 }
 
 document.getElementById("size").oninput = function(){
-   width  = this.value;
+    width = this.value;
+}
+
+document.getElementById("save_image").onclick = function(){
+    let image = cnv.toDataURL("image/jpg");
+    this.href = image;
 }
 
 cnv.onmousedown = (e) => {
